40a1063c0d4a04a6982f06b004b7efdad71ff360
Rodion02 <r.shkokov@g.nsu.ru>
2024-08-09 20:09:14 +0700

    new update

--- app.py
+++ app.py
@@ -4,36 +4,34 @@ from credentials import BOT_TOKEN, BOT_USERNAME, WEBAPP_URL
 import json
 
 
-async def launch_web_ui(update: Update, callback: CallbackContext):
-    # display our web-app!
+async def launch_web_ui(update: Update, context: CallbackContext):
+    # Отображаем веб-приложение
     kb = [
         [KeyboardButton(
-            "Show me my Web-App!",
+            "Показать веб-приложение!",
             web_app=WebAppInfo(WEBAPP_URL)
         )]
     ]
-    await update.message.reply_text("Let's do this...", reply_markup=ReplyKeyboardMarkup(kb))
+    await update.message.reply_text("Давайте начнем...", reply_markup=ReplyKeyboardMarkup(kb))
 
 
 async def web_app_data(update: Update, context: CallbackContext):
     data = json.loads(update.message.web_app_data.data)
-    await update.message.reply_text("Your data was:")
+    await update.message.reply_text("Ваши данные:")
     for result in data:
         await update.message.reply_text(f"{result['name']}: {result['value']}")
 
 
-
 if __name__ == '__main__':
-    # when we run the script we want to first create the bot from the token:
+    # Создаем бот из токена
     application = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    # and let's set a command listener for /start to trigger our Web UI
+    # Устанавливаем слушатель для команды /start для запуска веб-приложения
     application.add_handler(CommandHandler('start', launch_web_ui))
 
-    # as well as a web-app listener for the user-inputted data
+    # Слушатель для данных веб-приложения
     application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
 
-
-    # and send the bot on its way!
-    print(f"Your bot is listening! Navigate to http://t.me/{BOT_USERNAME} to interact with it!")
+    # Запуск бота
+    print(f"Ваш бот работает! Перейдите на http://t.me/{BOT_USERNAME}, чтобы взаимодействовать с ним!")
     application.run_polling()
